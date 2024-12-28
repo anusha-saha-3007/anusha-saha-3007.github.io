24a5849d9a65a32c1171d3fb4b3e3883804ce9fb
anusha-saha-3007 <anushasaha.xx@gmail.com>
2024-12-28 14:17:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add interactivity to the call-to-action button
+const ctaButton = document.getElementById("cta-button");
+
+ctaButton.addEventListener("click", () => {
+    alert("Thank you for getting started with ColorFlow!");
+});
